77b788d8d352350f5008da95fbf82c898655eeb2
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-08-04 02:46:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,27 +1,42 @@
 # code here!
+require "pry"
 class School
-  ROSTER = {}
+
   def initialize(school_name)
     @school_name = school_name
+    @roster = {}
   end
   
-  def roster
-    ROSTER
+  def roster 
+    @roster 
   end
   
   def add_student(name, grade)
     @student_name = name
     @student_grade = grade 
-  if ROSTER[grade] == nil 
-    ROSTER[grade] = []
-    ROSTER[grade] << name 
+  if @roster[grade] == nil 
+    @roster[grade] = []
+    @roster[grade] << name 
   else
-    ROSTER[grade] << name 
+    @roster[grade] << name 
     
     end
   end 
   
   def grade(grade)
-    return ROSTER[grade]
+    return @roster[grade]
+  end
+
+  def sort
+    sorted = @roster.to_a
+    sorted[0].sort
+    binding.pry
+    legoo = sorted.each do  |grade, value|
+      value[0] <=> value[1] 
+      
+    end 
+  
+    legoo.to_h
+
   end
 end
