7b5ae0806b1097cfda8519e5e096f7a74a91452b
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-08-04 02:58:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -28,7 +28,7 @@ class School
   end
 
   def sort
-    @roster.sort_by { |grade, name| name 
+    @roster.sort_by { |k, v| v[:age] }
     }
     sorted = @roster.to_a
     legoo = sorted.each do  |grade, value|
